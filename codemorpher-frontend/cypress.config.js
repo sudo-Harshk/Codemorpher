904c1fc524901f8a055fc5bbcf68a84f358d00b9
sudo-Harshk <harshk1744@gmail.com>
2025-05-05 07:36:07 +0530

    modified:   codemorpher-frontend/cypress.config.js 	modified:   codemorpher-frontend/cypress/e2e/copy_run.cy.js 	modified:   codemorpher-frontend/cypress/e2e/run.cy.js 	modified:   codemorpher-frontend/public/scripts/app.js 	modified:   codemorpher-frontend/public/styles/main.css

--- codemorpher-frontend/cypress.config.js
+++ codemorpher-frontend/cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:5500",  
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: false
+    supportFile: false,
+    defaultCommandTimeout: 40000,
+    responseTimeout: 40000
   },
 });
